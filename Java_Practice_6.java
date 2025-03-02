438ff206b79358c83869658f049643b8503d623f
amankumarsubudhi <aman.work404@gmail.com>
2025-03-02 09:09:39 +0530

    intial commit

---
+++ Java_Practice_6.java
@@ -0,0 +1,25 @@
+//Write a Program to Find Factorial of a Number in Java.
+
+import java.util.Scanner;
+
+public class Java_Practice_6 {
+
+    public static int Factorial(int n){
+        int fact=1;
+        while(n>=1){
+            fact = fact * n;
+            n--;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        int num,result;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter A Number = ");
+        num = sc.nextInt();
+        result = Factorial(num);
+        System.out.println("Factorial = " + result);
+    }
+    
+}
