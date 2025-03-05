438760704424e5848026b3a4ac900385886a3951
amankumarsubudhi <aman.work404@gmail.com>
2025-03-05 21:50:20 +0530

    intial commit

---
+++ Java_Practice_7.java
@@ -0,0 +1,35 @@
+//Write a Program to Calculate Simple Interest in Java
+
+import java.util.Scanner;
+
+public class Java_Practice_7 {
+
+    public static float si(int p,int t,float r){
+
+        float si = ((p*t*r) / 100);
+
+        return si;
+    }
+
+    public static void main(String[] args) {
+        int p,t;
+        float r,si;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter The Principal Amount = ");
+        p = sc.nextInt();
+
+        System.out.print("Enter The Rate Of Interest = ");
+        r = sc.nextInt();
+
+        System.out.print("Enter The Time Period (In Months) = ");
+        t = sc.nextInt();
+
+        si = si(p,t,r);
+
+        System.out.println("Simple Interest = " + si);
+
+    }
+    
+}
