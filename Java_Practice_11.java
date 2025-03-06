2c39cc96f50a15d7680ad5d23b92635d0c088db9
amankumarsubudhi <aman.work404@gmail.com>
2025-03-06 23:33:02 +0530

    intial commit

---
+++ Java_Practice_11.java
@@ -0,0 +1,22 @@
+// Write A Java Program To Print Diamond Star Pattern
+
+import java.util.Scanner;
+
+public class Java_Practice_11 {
+
+    public static void Diamond_Star_Pattern(int n){
+
+
+        
+    }
+
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Value Of N = ");
+        n = sc.nextInt();
+        sc.close();
+        Diamond_Star_Pattern(n);
+    }
+    
+}
