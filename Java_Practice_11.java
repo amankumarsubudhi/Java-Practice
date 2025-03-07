c0cca08cc541f1e81922bdf536b77472c91c5079
amankumarsubudhi <aman.work404@gmail.com>
2025-03-07 22:50:39 +0530

    intial commit

--- Java_Practice_11.java
+++ Java_Practice_11.java
@@ -6,6 +6,34 @@ public class Java_Practice_11 {
 
     public static void Diamond_Star_Pattern(int n){
 
+        int i,j;
+        int num = 1;
+
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n-1;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1;j<=2*i-1;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(i=n-1;i>=1;i--)
+        {
+            for(j=1;j<=n-1;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1;j<=2*n-1;j++)
+            {
+                System.out.println("*");
+            }
+            System.out.println();
+        }
 
         
     }
