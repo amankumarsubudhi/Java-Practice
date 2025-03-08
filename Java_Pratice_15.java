b1054cbea94fcac78aaf6b7a0239f3378aeda740
amankumarsubudhi <aman.work404@gmail.com>
2025-03-08 23:46:50 +0530

    intial commit

---
+++ Java_Pratice_15.java
@@ -0,0 +1,19 @@
+// Java Program To Find The Largest Element In The Array Using Sort Function
+
+import java.util.Arrays;
+
+public class Java_Pratice_15 {
+
+    public static void main(String[] args) {
+        int a[] = {4,6,2,74,58,24,23,33,18,5,6};
+        System.out.print("Displaying Elements Of Array = ");
+        for(int i = 0; i<a.length;i++)
+        {
+            System.out.print(a[i]+",");
+        }
+        System.out.println("");
+        Arrays.sort(a);
+        System.out.print("Largest = " + a[a.length - 1]);
+    }
+    
+}
